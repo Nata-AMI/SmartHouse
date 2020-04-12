78554f6fdb02a09717fa578232d4dbcf6ae4b7bb
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-04-12 16:18:31 +0500

    Initial commit

---
+++ src/main/java/smart/netology/ru/Radio.java
@@ -0,0 +1,58 @@
+package smart.netology.ru;
+
+public class Radio {
+    private final static int MAX_STATION = 10;
+    private final static int MIN_STATION = 0;
+    private final static int MAX_VOLUME = 100;
+    private final static int MIN_VOLUME = 0;
+    public int currentStation;
+    public int volume;
+
+    public void next() {
+        if (currentStation < MAX_STATION)
+            this.currentStation = currentStation + 1;
+        if (currentStation == MAX_STATION)
+            this.currentStation = 0;
+    }
+
+    public void prev() {
+        if (currentStation > MIN_STATION)
+            this.currentStation = currentStation - 1;
+        if (currentStation == MIN_STATION)
+            this.currentStation = 10;
+    }
+
+    public void increaseVolume() {
+        if (volume < MAX_VOLUME)
+            this.volume = volume + 1;
+        if (volume == MAX_VOLUME)
+            this.volume = volume;
+    }
+
+    public void decreaseVolume() {
+        if (volume > MIN_VOLUME)
+            this.volume = volume - 1;
+        if (volume == MIN_VOLUME)
+            this.volume = volume;
+    }
+
+    public void setStation(int currentStation) {
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+}
+
+
+
---
+++ src/test/java/smart/netology/ru/RadioTest.java
@@ -0,0 +1,39 @@
+package smart.netology.ru;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RadioTest {
+
+    @Test
+    void next() {
+        Radio radio = new Radio();
+        radio.setStation(7);
+        radio.next();
+        assertEquals(8, radio.getCurrentStation());
+}
+    @Test
+    void prev() {
+        Radio radio = new Radio();
+        radio.setStation(7);
+        radio.prev();
+        assertEquals(6, radio.getCurrentStation());
+    }
+
+    @Test
+    void decreaseVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(7);
+        radio.decreaseVolume();
+        assertEquals(6, radio.getVolume());
+    }
+
+    @Test
+    void increaseVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(7);
+        radio.increaseVolume();
+        assertEquals(8, radio.getVolume());
+    }
+}
