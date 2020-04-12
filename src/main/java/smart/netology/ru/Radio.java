b7088aa8a9605013b00436d2f9f9389bb3d6e8a1
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-04-12 23:35:11 +0500

    Revert "Lombok"
    This reverts commit 19bd7a63cb888160948f5ee59e0ab7b4254e2bbf.

--- src/main/java/smart/netology/ru/Radio.java
+++ src/main/java/smart/netology/ru/Radio.java
@@ -1,12 +1,5 @@
 package smart.netology.ru;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class Radio {
     private final static int MAX_STATION = 10;
     private final static int MIN_STATION = 0;
@@ -15,6 +8,49 @@ public class Radio {
     public int currentStation;
     public int volume;
 
+    public void next() {
+        if (currentStation < MAX_STATION)
+            this.currentStation = currentStation + 1;
+        if (currentStation == MAX_STATION)
+            this.currentStation = 0;
+    }
+
+    public void prev() {
+        if (currentStation > MIN_STATION)
+            this.currentStation = currentStation - 1;
+        if (currentStation == MIN_STATION)
+            this.currentStation = 10;
+    }
+
+    public void increaseVolume() {
+        if (volume < MAX_VOLUME)
+            this.volume = volume + 1;
+        if (volume == MAX_VOLUME)
+            this.volume = volume;
+    }
+
+    public void decreaseVolume() {
+        if (volume > MIN_VOLUME)
+            this.volume = volume - 1;
+        if (volume == MIN_VOLUME)
+            this.volume = volume;
+    }
+
+    public void setStation(int currentStation) {
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
 
 }
 
