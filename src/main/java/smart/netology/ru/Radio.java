68225182c901e3ed2f104ded43e33be600eb7ec2
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-04-16 23:08:51 +0500

    Master1

--- src/main/java/smart/netology/ru/Radio.java
+++ src/main/java/smart/netology/ru/Radio.java
@@ -1,6 +1,61 @@
 package smart.netology.ru;
 
+package smart.netology.ru;
 
+
+public class Radio {
+    private final static int MAX_STATION = 10;
+    private final static int MIN_STATION = 0;
+    private final static int MAX_VOLUME = 100;
+    private final static int MIN_VOLUME = 0;
+    public int currentStation;
+    public int volume;
+
+    public void next() {
+        if (currentStation < MAX_STATION)
+            this.currentStation = currentStation + 1;
+        if (currentStation == MAX_STATION)
+            this.currentStation = 0;
+    }
+
+    public void prev() {
+        if (currentStation > MIN_STATION)
+            this.currentStation = currentStation - 1;
+        if (currentStation == MIN_STATION)
+            this.currentStation = 10;
+    }
+
+    public void increaseVolume() {
+        if (volume < MAX_VOLUME)
+            this.volume = volume + 1;
+        if (volume == MAX_VOLUME)
+            this.volume = volume;
+    }
+
+    public void decreaseVolume() {
+        if (volume > MIN_VOLUME)
+            this.volume = volume - 1;
+        if (volume == MIN_VOLUME)
+            this.volume = volume;
+    }
+
+    public void setStation(int currentStation) {
+        if (currentStation <= MAX_STATION && currentStation >= MIN_STATION) {
+            this.currentStation = currentStation;
+        } else {
+            this.currentStation = 0;
+        }
+    }
+
+
+    public void setVolume(int volume) {
+        if (volume <= 100 && volume >= 0) {
+            this.volume = volume;
+        } else {
+            volume = 0;
+        }
+    }
+}
 
 
 public class Radio {
